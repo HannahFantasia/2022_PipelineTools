321a4c596b0a089a74b0e221fde0cd0259fcd17f
HannahFantasia <49023943+HannahFantasia@users.noreply.github.com>
2024-02-18 01:11:26 +0100

    Update __init__.py

--- Krita_Plugins/Biggenvlees/biggenvlees/__init__.py
+++ Krita_Plugins/Biggenvlees/biggenvlees/__init__.py
@@ -1,7 +1,7 @@
-from .biggenvlees import Biggenvlees
+from .CopyPaste_Frame_Board import CopyPaste_Frame_Board
 
 # And add the extension to Krita's list of extensions:
 app = Krita.instance()
 # Instantiate your class:
-extension = Biggenvlees(parent = app)
+extension = CopyPaste_Frame_Board(parent = app)
 app.addExtension(extension)
